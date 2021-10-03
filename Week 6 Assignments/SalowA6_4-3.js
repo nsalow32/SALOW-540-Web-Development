ffeab1a755e250c2c229cb9c594d2aa981be588f
Nicole <90110876+nsalow32@users.noreply.github.com>
2021-10-03 10:03:23 -0500

    fixed it

--- Week 6 Assignments/SalowA6_4-3.js
+++ Week 6 Assignments/SalowA6_4-3.js
@@ -6,7 +6,7 @@ function numprompt() {
     var num1 = prompt("Enter the first of three numbers:");
     var num2 = prompt("Enter the second of three numbers:");
     var num3 = prompt("Enter the third of three numbers:");
-}
-function mathmax() {
-    math.max(var num1, var num2, var num3);
+    var mathmax = Math.max(num1, num2, num3)
+
+    alert(mathmax);
 }
