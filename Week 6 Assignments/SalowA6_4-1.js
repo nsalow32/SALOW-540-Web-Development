5358e21e9719c1c0e031bd81dbd80143373332c7
Nicole <90110876+nsalow32@users.noreply.github.com>
2021-10-03 11:01:42 -0500

    fixes

--- Week 6 Assignments/SalowA6_4-1.js
+++ Week 6 Assignments/SalowA6_4-1.js
@@ -1,3 +1,81 @@
 // SalowA6_4-1.js
 
-var n = 0;
+
+function numcube() {
+    var n1 = 1;
+    var n1squared = Math.pow(n1,2)
+    var n1cubed = Math.pow(n1,3)
+    alert(n1 + " squared is " + n1squared + ", " + n1 + " cubed is" + n1cubed);
+
+    var n2 = 2;
+    var n2squared = Math.pow(n2,2)
+    var n2cubed = Math.pow(n2,3)
+    alert(n2 + " squared is " + n2squared + ", " + n2 + " cubed is " + n2cubed);
+
+    var n3 = 3;
+    var n3squared = Math.pow(n3,2)
+    var n3cubed = Math.pow(n3,3)
+    alert(n3 + " squared is " + n3squared + ", " + n3 + " cubed is "+ n3cubed);
+
+    var n4 = 4;
+    var n4squared = Math.pow(n4,2)
+    var n4cubed = Math.pow(n4,3)
+    alert(n4 + " squared is " + n4squared + ", " + n4 + " cubed is "+ n4cubed);
+
+    var n5 = 5;
+    var n5squared = Math.pow(n5,2)
+    var n5cubed = Math.pow(5,3)
+    alert(n5 + " squared is " + n5squared  + ", " + n5 + " cubed is "+ n5cubed);
+
+    var n6 = 6;
+    var n6squared = Math.pow(n6,2)
+    var n6cubed = Math.pow(n6,3)
+    alert(n6 + " squared is " + n6squared  + ", " + n6 + " cubed is "+ n6cubed);
+
+    var n7 = 7;
+    var n7squared = Math.pow(n7,2)
+    var n7cubed = Math.pow(n7,3)
+    alert(n7 + " squared is " + n7squared  + ", " + n7 + " cubed is "+ n7cubed);
+
+    var n8 = 8;
+    var n8squared = Math.pow(n8,2)
+    var n8cubed = Math.pow(n8,3)
+    alert(n8 + " squared is " + n8squared  + ", " + n8 + " cubed is " + n8cubed);
+
+    var n9 = 9;
+    var n9squared = Math.pow(n9,2)
+    var n9cubed = Math.pow(n9,3)
+    alert(n9 + " squared is " + n9squared + ", " + n9 + " cubed is " + n9cubed);
+
+    var n10 = 10;
+    var n10squared = Math.pow(n10,2)
+    var n10cubed = Math.pow(n10,3)
+    alert(n10 + " squared is " + n10squared + ", " + n10 + " cubed is " + n10cubed);
+
+    var n11 = 11;
+    var n11squared = Math.pow(n11,2)
+    var n11cubed = Math.pow(n11,3)
+    alert(n11 + " squared is " + n11squared + ", " + n11 + " cubed is " + n11cubed);
+
+    var n12 = 12;
+    var n12squared = Math.pow(n12,2)
+    var n12cubed = Math.pow(n12,3)
+    alert(n12 + " squared is " + n12squared + ", " + n12 + " cubed is " + n12cubed);
+
+    var n13 = 13;
+    var n13squared = Math.pow(n13,2)
+    var n13cubed = Math.pow(n13,3)
+    alert(n13 + " squared is " + n13squared + ", " + n13 + " cubed is " + n13cubed);
+
+    var n14 = 14;
+    var n14squared = Math.pow(n14,2)
+    var n14cubed = Math.pow(n14,3)
+    alert(n14 + " squared is " + n14squared + ", " + n14 + " cubed is " + n14cubed);
+
+    var n15 = 15;
+    var n15squared = Math.pow(n15,2)
+    var n15cubed = Math.pow(n15,3)
+    alert(n15 + " squared is " + n15squared + ", " + n15 + " cubed is " + n15cubed);
+
+
+}
